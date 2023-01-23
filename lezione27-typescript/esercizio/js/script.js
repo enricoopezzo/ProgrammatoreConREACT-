49ff89f2e91a7ae1fd294b401651b8a148170501
emanueleumberto <info@emanueleumberto.it>
2023-01-23 18:14:17 +0100

    Soluzione esercizio

---
+++ lezione27-typescript/esercizio/js/script.js
@@ -0,0 +1,46 @@
+"use strict";
+let count = 1;
+class Todo {
+    constructor(txt) {
+        this.id = count++;
+        this.txt = txt;
+        this.complete = "";
+        this.data = new Date();
+    }
+}
+const listaTodo = [];
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.querySelector('#addTodo button');
+    btn === null || btn === void 0 ? void 0 : btn.addEventListener('click', addTodo);
+    printTodos();
+});
+function addTodo() {
+    const inputTodo = document.querySelector('#addTodo input');
+    listaTodo.push(new Todo(inputTodo === null || inputTodo === void 0 ? void 0 : inputTodo.value));
+    printTodos();
+}
+function printTodos() {
+    let ul = document.querySelector('#listaTodo ul');
+    if (ul !== null) {
+        ul.innerHTML = "";
+    }
+    listaTodo.forEach((t, i) => {
+        const li = document.createElement('li');
+        li.setAttribute('class', t.complete);
+        li.innerHTML = `${t.id} - ${t.txt} 
+                        <span>${t.data.getDate()}/${t.data.getMonth()}</span> 
+                        <span>
+                            <button type="button" class="btn btn-danger" onclick="removeTodo(${i})">x</button>
+                            <button type="button" class="btn btn-warning" onclick="completeTodo(${i})">o</button>
+                        </span>`;
+        ul === null || ul === void 0 ? void 0 : ul.appendChild(li);
+    });
+}
+function removeTodo(index) {
+    listaTodo.splice(index, 1);
+    printTodos();
+}
+function completeTodo(index) {
+    listaTodo[index].complete = 'complete';
+    printTodos();
+}
---
+++ lezione27-typescript/esercizio/ts/script.ts
@@ -0,0 +1,57 @@
+let count = 1;
+
+class Todo {
+    id: number;
+    txt: string;
+    complete: string;
+    data: Date;
+    constructor(txt: string) {
+        this.id = count++;
+        this.txt = txt;
+        this.complete = "";
+        this.data = new Date();
+    }
+}
+
+const listaTodo: Todo[] = []
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.querySelector('#addTodo button')
+    btn?.addEventListener('click', addTodo);
+    printTodos();
+})
+
+function addTodo(): void {
+    const inputTodo = document.querySelector('#addTodo input') as HTMLInputElement
+    listaTodo.push(new Todo(inputTodo?.value))
+    printTodos();
+}
+
+function printTodos(): void {
+    let ul = document.querySelector('#listaTodo ul');
+    if(ul !== null){
+        ul.innerHTML = ""; 
+    }
+    
+    listaTodo.forEach((t,i) => {
+        const li = document.createElement('li')
+        li.setAttribute('class', t.complete)
+        li.innerHTML = `${t.id} - ${t.txt} 
+                        <span>${t.data.getDate()}/${t.data.getMonth()}</span> 
+                        <span>
+                            <button type="button" class="btn btn-danger" onclick="removeTodo(${i})">x</button>
+                            <button type="button" class="btn btn-warning" onclick="completeTodo(${i})">o</button>
+                        </span>`;
+        ul?.appendChild(li);
+    })
+}
+
+function removeTodo(index: number): void {
+    listaTodo.splice(index, 1);
+    printTodos();
+}
+
+function completeTodo(index: number): void {
+    listaTodo[index].complete = 'complete'
+    printTodos();
+}
